b4c2e3fbdc852f30eab7615059ca37663a136b49
ZuoSyuanWang <50255139+zuosyuanwang@users.noreply.github.com>
2020-03-12 23:18:32 +0800

    1.TwoSum AC, 72, 100

---
+++ 1.two-sum.c
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=1 lang=c
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int *twoSum(int *nums, int numsSize, int target, int *returnSize)
+{
+    for (int i = 0; i < numsSize - 1; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                int *result = (int *)malloc(sizeof(int) * 2);
+                if (result == NULL)
+                {
+                    *returnSize = 0;
+                    return NULL;
+                }
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
+
+// @lc code=end
