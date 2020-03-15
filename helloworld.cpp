05f1bf58c7c80f2369aef7c99273159477a968a2
ZuoSyuanWang <50255139+zuosyuanwang@users.noreply.github.com>
2020-03-16 02:07:34 +0800

    add test file helloworld.cpp

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a % 10 << endl;
+    // system("PAUSE");
+}
+
+
+
