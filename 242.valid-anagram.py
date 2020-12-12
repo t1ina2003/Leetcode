f6fb4b1e978c1e914caefd90591c226ad53312bb
ZuoSyuanWang <zuosyuanwang@gmail.com>
2020-12-13 01:27:58 +0800

    [242] 新增python解

---
+++ 242.valid-anagram.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=242 lang=python
+#
+# [242] Valid Anagram
+#
+
+# @lc code=start
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s = sorted(s)
+        t = sorted(t)
+        return s == t
+# @lc code=end
+
