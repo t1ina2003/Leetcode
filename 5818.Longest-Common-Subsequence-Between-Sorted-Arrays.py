cde554cd5be4a1a17b148347746a0559e0acf640
ZuoSyuanWang <zuosyuanwang@gmail.com>
2021-07-02 19:00:57 +0800

    [update] 5818 two func edition

--- 5818.Longest-Common-Subsequence-Between-Sorted-Arrays.py
+++ 5818.Longest-Common-Subsequence-Between-Sorted-Arrays.py
@@ -7,7 +7,11 @@
 # @lc code=start
 class Solution:
     def longestCommomSubsequence(self, arrays: List[List[int]]) -> List[int]:
-        # result [7,8] will error in [8,7] without sorted function.
-        return sorted(set(arrays[0]).intersection(*arrays[1:]))
+        result = arrays[0]
+        for i in range(1,len(arrays)):
+            result = self.findIntersection(result,arrays[i])
+        return sorted(result)
+    def findIntersection(self, list1: List[int], list2: List[int]) -> List[int]:
+        return list(set(list1) & set(list2))
 # @lc code=end
 
